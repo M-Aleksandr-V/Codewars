867ee1389fe730c05cb7f475813251f99ff9d842
M-Aleksandr-V <morozov.freeze89@yandex.ru>
2021-11-18 19:43:43 +0300

    Update find_it.py

--- find_it.py
+++ find_it.py
@@ -1,7 +1,10 @@
 from collections import Counter
 
 def find_it(seq):
-    return  Counter(seq)
+    __dict__ = dict(Counter(seq))
+    for k, v in __dict__.items():
+        if v % 2 == 1:
+            return  k
 
 seq = [20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]
 print(find_it(seq))
