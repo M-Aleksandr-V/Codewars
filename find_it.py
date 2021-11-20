b2c022b3ef7305a8ca039f4bde82b23eef2155fc
M-Aleksandr-V <morozov.freeze89@yandex.ru>
2021-11-20 08:08:57 +0300

    Update find_it.py

--- find_it.py
+++ find_it.py
@@ -1,7 +1,8 @@
 def find_it(seq):
     for i in seq:
         if seq.count(i) % 2 != 0:
-            return  i
+            return i
 
-seq = [20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]
-print(find_it(seq))
+
+a = [20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]
+print(find_it(a))
